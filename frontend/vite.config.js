1529a47e28ae1885cbb39c9a25261f53f1089d75
gdatarealstate <gdatarealestate@gmail.com>
2025-09-09 11:02:10 -0600

    Actualización Prueba Vite 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',  // Indica la raíz del proyecto donde está el index.html
   server: {
     proxy: {
       '/api': {
@@ -13,5 +15,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 });
